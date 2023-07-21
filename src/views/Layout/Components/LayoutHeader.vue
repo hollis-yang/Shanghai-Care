6b473f5cb51d9d642bfe21218ec14e833e34cde9
hollis-yang <hollis2002@qq.com>
2023-07-21 18:47:20 +0800

    LayoutHeader添加路由

--- src/views/Layout/Components/LayoutHeader.vue
+++ src/views/Layout/Components/LayoutHeader.vue
@@ -31,21 +31,30 @@ onMounted(() => {
   <div class="header-container">
     <!-- 左侧 -->
     <div class="left">
+
       <!-- 主页概述 -->
-      <div class="left-item active">
-        <img src="../../../assets/img/bntactive.png" alt="概述">
-        <span class="desc">主 页 概 述</span>
-      </div>
+      <RouterLink to="/screen">
+        <div class="left-item active">
+          <img src="../../../assets/img/bntactive.png" alt="概述">
+          <span class="desc">主 页 概 述</span>
+        </div>
+      </RouterLink>
+
       <!-- 养老设施页 -->
-      <div class="left-item">
-        <img src="../../../assets/img/bnt.png" alt="养老设施">
-        <span class="desc">养 老 设 施</span>
-      </div>
+      <RouterLink to="/facility">
+        <div class="left-item">
+          <img src="../../../assets/img/bnt.png" alt="养老设施">
+          <span class="desc">养 老 设 施</span>
+        </div>
+      </RouterLink>
+
       <!-- 护工信息页 -->
-      <div class="left-item">
-        <img src="../../../assets/img/bnt.png" alt="护理员信息">
-        <span class="desc">护 工 信 息</span>
-      </div>
+      <RouterLink to="/nursing">
+        <div class="left-item">
+          <img src="../../../assets/img/bnt.png" alt="护理员信息">
+          <span class="desc">护 工 信 息</span>
+        </div>
+      </RouterLink>
     </div>
 
     <!-- 用于flex布局占位 -->
@@ -53,16 +62,22 @@ onMounted(() => {
 
     <!-- 右侧 -->
     <div class="right">
+
       <!-- 社区活动页 -->
-      <div class="right-item active">
-        <img src="../../../assets/img/bnt.png" alt="概述">
-        <span class="desc">社 区 活 动</span>
-      </div>
+      <RouterLink to="/activity">
+        <div class="right-item">
+          <img src="../../../assets/img/bnt.png" alt="概述">
+          <span class="desc">社 区 活 动</span>
+        </div>
+      </RouterLink>
+
       <!-- 健康档案页 -->
-      <div class="right-item">
-        <img src="../../../assets/img/bnt.png" alt="养老设施">
-        <span class="desc">健 康 档 案</span>
-      </div>
+      <RouterLink to="health">
+        <div class="right-item">
+          <img src="../../../assets/img/bnt.png" alt="养老设施">
+          <span class="desc">健 康 档 案</span>
+        </div>
+      </RouterLink>
       <!-- 最右侧时间 -->
       <div class="right-item datetime">
         <div class="desc">{{ currentTime }}</div>
