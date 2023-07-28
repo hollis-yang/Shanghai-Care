c5f017299cd421f52cba3ab310f1a268ed1ba028
hollis-yang <hollis2002@qq.com>
2023-07-28 13:12:34 +0800

    elementplus自适应

--- src/views/Nursing/components/ChangeRoute.vue
+++ src/views/Nursing/components/ChangeRoute.vue
@@ -4,21 +4,30 @@ import { DataAnalysis, Files } from '@element-plus/icons-vue'
 
 <template>
   <el-row class="mb-4">
-    <el-button color="#2642AA" style="width: 9vw;">
-      <el-icon><DataAnalysis /></el-icon>
-      &nbsp;护工数据可视化
+    <el-button color="#2642AA" class="el-button">
+      <span class="icon"><el-icon><DataAnalysis /></el-icon></span>
+      <span class="desc">&nbsp;护工数据可视化</span>
     </el-button>
 
     <span style="width: 1vw;"></span>
     
     <router-link :to="{ path: '/nursing/query' }">
-      <el-button color="#2642AA" plain style="width: 9vw;">
-        <el-icon><Files /></el-icon>
-        &nbsp;护理员信息查询
+      <el-button color="#2642AA" plain class="el-button">
+        <span class="icon"><el-icon><Files /></el-icon></span>
+        <span class="desc">&nbsp;护理员信息查询</span>
       </el-button>
     </router-link>
 
   </el-row>
 </template>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.el-button {
+  width: 9vw;
+  height: 4vh;
+  .desc,
+  .icon {
+    font-size: 0.85vw;
+  }
+}
+</style>
--- src/views/NursingQuery/components/ChangeRoute.vue
+++ src/views/NursingQuery/components/ChangeRoute.vue
@@ -5,23 +5,34 @@ import { DataAnalysis, Files } from '@element-plus/icons-vue'
 <template>
   <el-row class="mb-4">
     <router-link :to="{ path: '/nursing' }">
-      <el-button color="#2642AA" plain style="width: 9vw;">
-        <el-icon>
-          <DataAnalysis />
-        </el-icon>
-        &nbsp;护工数据可视化
+      <el-button color="#2642AA" class="el-button" plain>
+        <span class="icon"><el-icon>
+            <DataAnalysis />
+          </el-icon></span>
+        <span class="desc">&nbsp;护工数据可视化</span>
       </el-button>
     </router-link>
 
     <span style="width: 1vw;"></span>
 
-    <el-button color="#2642AA" style="width: 9vw;">
-      <el-icon>
-        <Files />
-      </el-icon>
-      &nbsp;护理员信息查询
+    <el-button color="#2642AA" class="el-button">
+      <span class="icon"><el-icon>
+          <Files />
+        </el-icon></span>
+      <span class="desc">&nbsp;护理员信息查询</span>
     </el-button>
+
   </el-row>
 </template>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.el-button {
+  width: 9vw;
+  height: 4vh;
+
+  .desc,
+  .icon {
+    font-size: 0.85vw;
+  }
+}
+</style>
