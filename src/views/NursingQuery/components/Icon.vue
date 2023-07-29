a13a882e08f18f1083a5712edf45c7b303d2c7e3
JocelynGLi <2532195073@qq.com>
2023-07-29 11:44:55 +0800

    设置图标

---
+++ src/views/NursingQuery/components/Icon.vue
@@ -0,0 +1,11 @@
+<template>
+    <Components/>
+</template>
+<script setup lang="ts">
+import { ref, toRefs, inject } from 'vue';
+
+const props = defineProps({
+  Components: String,
+})
+const { Components } = toRefs(props)
+</script>
