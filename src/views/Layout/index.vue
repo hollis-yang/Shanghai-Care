c0dc1794cf052f1d6a11e73e90f6e13b502e4761
hollis-yang <hollis2002@qq.com>
2023-07-31 13:30:54 +0800

    主页基本布局和less封装

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,18 +1,50 @@
 <script setup>
-import LayoutHeader from './Components/LayoutHeader.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutAge from './components/LayoutAge.vue'
+import LayoutDistrict from './components/LayoutDistrict.vue'
+import LayoutExpectLife from './components/LayoutExpectLife.vue'
+import LayoutInfo from './components/LayoutInfo.vue'
+import LayoutMap from './components/LayoutMap.vue'
+import LayoutPie from './components/LayoutPie.vue'
+import LayoutTable from './components/LayoutTable.vue'
 </script>
 
 <template>
   <div class="layout-container">
     <div class="layout-header">
-      <LayoutHeader></LayoutHeader>
+      <LayoutHeader class="chart"></LayoutHeader>
+    </div>
+
+    <div class="layout-info">
+      <LayoutInfo class="chart"></LayoutInfo>
+    </div>
+
+    <div class="layout-age">
+      <LayoutAge class="chart"></LayoutAge>
+    </div>
+
+    <div class="layout-expect-life">
+      <LayoutExpectLife class="chart"></LayoutExpectLife>
+    </div>
+
+    <div class="layout-map">
+      <LayoutMap class="chart"></LayoutMap>
+    </div>
+
+    <div class="layout-district">
+      <LayoutDistrict class="chart"></LayoutDistrict>
+    </div>
+
+    <div class="layout-table">
+      <LayoutTable class="chart"></LayoutTable>
+    </div>
+
+    <div class="layout-pie">
+      <LayoutPie class="chart"></LayoutPie>
     </div>
   </div>
 </template>
 
 <style scoped lang="less">
-.layout-header {
-  width: 100%;
-  height: 10vh;
-}
+@import '../../assets/css/layout.less';
 </style>
