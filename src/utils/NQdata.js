780bc6d57c65c152109f673554efd76bcece745c
JocelynGLi <2532195073@qq.com>
2023-08-03 10:28:52 +0800

    NursingQuery筛选下拉框

---
+++ src/utils/NQdata.js
@@ -0,0 +1,156 @@
+ // 下拉款数据1
+export const conditionSelect = [
+  {
+    label: '全部',
+    id: '9',
+  },
+  {
+    label: '性别',
+    id: '1',
+  },
+  {
+    label: '年龄',
+    id: '2',
+  },
+  {
+    label: '户籍情况',
+    id: '3',
+  },
+  {
+    label: '持证情况',
+    id: '4',
+  },
+  {
+    label: '综合评价等级',
+    id: '5',
+  },
+]
+// 下拉款数据2
+export const gender = [
+  {
+    label: '全部',
+    id: '9',
+  },
+  {
+    label: '男',
+    id: '1',
+  },
+  {
+    label: '女',
+    id: '2',
+  },
+];
+export const age = [
+  {
+    label: '全部',
+    id: '9',
+  },
+  {
+    label: '29岁及以下',
+    id: '1',
+  },
+  {
+    label: '30-39岁',
+    id: '2',
+  },
+  {
+    label: '40-49岁',
+    id: '3',
+  },
+  {
+    label: '50-59岁',
+    id: '4',
+  },
+  {
+    label: '60岁以上',
+    id: '5',
+  },
+]
+export const domicile = [
+  {
+    label: '全部',
+    id: '9',
+  },
+  {
+    label: '本市户籍',
+    id: '1',
+  },
+  {
+    label: '非本市户籍',
+    id: '2',
+  },
+];
+export const situation = [
+  {
+    label: '全部',
+    id: '9',
+  },
+  {
+    label: '养老护理员高级',
+    id: '1',
+  },
+  {
+    label: '养老护理员中级',
+    id: '2',
+  },
+  {
+    label: '养老护理员初级',
+    id: '3',
+  },
+  {
+    label: '养老护理员高级技师',
+    id: '4',
+  },
+  {
+    label: '养老护理员技师',
+    id: '5',
+  },
+  {
+    label: '养老护理员（医疗照护）技能水平评价证明',
+    id: '6',
+  },
+  {
+    label: '无',
+    id: '7',
+  },
+]
+export const grade = [
+  {
+    label: '全部',
+    id: '9',
+  },
+  {
+    label: '一级',
+    id: '1',
+  },
+  {
+    label: '二级',
+    id: '2',
+  },
+  {
+    label: '三级',
+    id: '3',
+  },
+  {
+    label: '四级',
+    id: '4',
+  },
+  {
+    label: '五级',
+    id: '5',
+  },
+  {
+    label: '六级',
+    id: '6',
+  },
+  {
+    label: '七级',
+    id: '7',
+  },
+]
+export const some = [
+  {
+    label: '全部',
+    id: '9',
+  },
+]
