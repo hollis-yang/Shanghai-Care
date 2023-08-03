248bc016cfcb1fa60a810b48ed9f7324e54e5a82
hollis-yang <hollis2002@qq.com>
2023-08-03 13:11:54 +0800

    行政区查询title

---
+++ src/utils/district.js
@@ -0,0 +1,70 @@
+export const districtOptions = [
+  {
+    value: 'Option0',
+    label: '全市',
+  },
+  {
+    value: 'Option1',
+    label: '黄浦区',
+  },
+  {
+    value: 'Option2',
+    label: '徐汇区',
+  },
+  {
+    value: 'Option3',
+    label: '长宁区',
+  },
+  {
+    value: 'Option4',
+    label: '静安区',
+  },
+  {
+    value: 'Option5',
+    label: '普陀区',
+  },
+  {
+    value: 'Option6',
+    label: '虹口区',
+  },
+  {
+    value: 'Option7',
+    label: '杨浦区',
+  },
+  {
+    value: 'Option8',
+    label: '闵行区',
+  },
+  {
+    value: 'Option9',
+    label: '宝山区',
+  },
+  {
+    value: 'Option10',
+    label: '嘉定区',
+  },
+  {
+    value: 'Option11',
+    label: '浦东新区',
+  },
+  {
+    value: 'Option12',
+    label: '金山区',
+  },
+  {
+    value: 'Option13',
+    label: '松江区',
+  },
+  {
+    value: 'Option14',
+    label: '青浦区',
+  },
+  {
+    value: 'Option15',
+    label: '奉贤区',
+  },
+  {
+    value: 'Option16',
+    label: '崇明区',
+  },
+]
--- src/views/Layout/Components/LayoutTable.vue
+++ src/views/Layout/Components/LayoutTable.vue
@@ -1,7 +1,34 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { districtOptions } from '@/utils/district'
+
+const selectorValue = ref('Option0')
+
+
+</script>
 
 <template>
-  <div></div>
+  <div class="select-district">
+    <span class="desc">丨各行政区信息查询</span>
+    <el-select v-model="selectorValue" class="m-2 elp-select" size="large">
+      <el-option v-for="item in districtOptions" :key="item.value" :label="item.label" :value="item.value" />
+    </el-select>
+  </div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.select-district {
+  .desc {
+    display: inline-block;
+    width: 10vw;
+    font-size: 1.1vw;
+    font-family: Microsoft YaHei;
+    font-weight: 700;
+    margin-right: 2vw;
+  }
+  .elp-select {
+    width: 12vw;
+    height: 4.3vh;
+  }
+}
+</style>
