6db7d6500a3ac71be1860eee45e86b634c34b1a2
hollis-yang <hollis2002@qq.com>
2023-08-06 13:48:53 +0800

    左上角信息展示初步

--- src/views/Layout/Components/LayoutInfo.vue
+++ src/views/Layout/Components/LayoutInfo.vue
@@ -1,7 +1,77 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <div class="no">
+    <div class="no-hd">
+      <ul>
+        <li>24870895</li>
+        <li>5815462</li>
+      </ul>
+    </div>
+    <div class="no-bd">
+      <ul>
+        <li>上海市人口总数</li>
+        <li>60岁以上老年人口数</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+ul {
+  padding: 0;
+
+  li {
+    list-style: none;
+  }
+}
+
+@font-face {
+  font-family: electronicFont;
+  src: url('../../../assets/font/DS-DIGIT.TTF');
+}
+
+.no .no-hd {
+  position: relative;
+  border: 1px solid transparent;
+
+  ul {
+    display: flex;
+
+    li {
+      position: relative;
+      flex: 1;
+      text-align: center;
+      // 居中
+      height: 5vh;
+      line-height: 5vh;
+      font-size: 5vh;
+      color: #ffeb7b;
+      font-family: electronicFont;
+      font-weight: bold;
+
+      &:first-child::after {
+        content: "";
+        position: absolute;
+        height: 90%;
+        width: 0.1vw;
+        background: rgba(255, 255, 255, 0.7);
+        right: -3%;
+      }
+    }
+  }
+}
+
+.no .no-bd ul {
+  display: flex;
+
+  li {
+    flex: 1;
+    height: 1vh;
+    line-height: 1vh;
+    text-align: center;
+    font-size: 1.6vh;
+    color: #fff
+  }
+}
+</style>
