98ad6fdb7fd19328f6ffa2b39319727e2dca460a
zh <camellia030621@gmail.com>
2023-08-10 01:22:48 +0800

    引入ArcGIS主题样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import "@arcgis/core/assets/esri/themes/dark/main.css"
 
 import App from './App.vue'
 import router from './router'
