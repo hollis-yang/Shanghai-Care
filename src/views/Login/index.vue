4db44f9247ae892478be839e723cad14c7922eca
hollis-yang <hollis2002@qq.com>
2023-08-10 00:17:38 +0800

    login初步(分辨率适配待调整)

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,20 +1,44 @@
 <script setup>
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+  // innerHtml带标签; innerText仅文字
+  label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)  // 每个字母动画的延迟
+    .join('')
+})
 </script>
 
 <template>
-  <div class="login">
+  <div id="app">
+    <div class="container">
+      <h1>上海市智慧养老系统</h1>
+      <form>
+        <div class="form-control">
+          <!-- 必填项 required -->
+          <input type="text" required>
+          <label>
+            <span v-for="(item, index) in '用户名 / Username'" :key="index" :style="`transition-delay:${index * 30}ms`">
+              {{ item }}</span>
+          </label>
+        </div>
 
+        <div class="form-control">
+          <!-- 必填项 required -->
+          <input type="password" required>
+          <label>
+            <span v-for="(item, index) in '密码 / Password'" :key="index" :style="`transition-delay:${index * 30}ms`">
+              {{ item }}</span>
+          </label>
+        </div>
+
+        <button class="btn">登录</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-/* 覆盖global.less中的背景 */
-.login {
-  background: url(../../assets/img/login.jpg);
-  background-repeat: no-repeat;
-  background-position: 0 0;
-  background-size: 100% 100%;
-  width: 100%;
-  height: 100%;
-}
+@import '../../assets/css/login.less';
 </style>
